872d89c2677870eb1b66347bab4645c63b59a8f5
Gant2134 <Sonic25100@yahoo.com>
2017-01-11 13:13:27 -0800

    New assigment
    that new stuff decribing class module

---
+++ Tax Application/Chapter 11 code/Chapter 11 code/Chapter 11 code/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_11_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is how we declare a new object of the class person
+            //this is also reffered to as instanciating an object of the class person
+            //the first part "person p" declaring a reference to an object of the class person
+            // the second part "new person" creating an object of the class "person"
+            person p = new person();
+
+            p.m_FirstName = "Christopher";
+            p.m_LastName = "McCall";
+            p.m_age = 30;
+
+            // the line below will not compile because since we did not specify any access
+            // modifier for our m_Haircolor, the defaul access is private, m_Haircolor member Variable
+
+            //p.m_Haircolor = "Dark";
+        }
+    }
+}
