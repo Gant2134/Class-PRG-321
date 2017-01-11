77b62fa9b5abdcfc7cd334f7fec0684ae697c180
Gant2134 <Sonic25100@yahoo.com>
2017-01-11 13:23:20 -0800

    discussing that stuff about classes and public seaking

--- Tax Application/Chapter 11 code/Chapter 11 code/Chapter 11 code/person.cs
+++ Tax Application/Chapter 11 code/Chapter 11 code/Chapter 11 code/person.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Chapter_11_code
 {
-    class person
+    // we make person public, meaning its accessible from other modules can access and use class
+    public class person
     {
+        //the next three variables are refered to as "member variables" of the class person
+        // in other words, those memebr variables are also reffered to as properties
+        //the protected attribute
+
+        //if the member variables are not specified as either public protected or private
+        //the default is private.
+        public string m_FirstName;
+        public string m_LastName;
+        public int m_age;
+
+        string m_Haircolor;
+
     }
 }
