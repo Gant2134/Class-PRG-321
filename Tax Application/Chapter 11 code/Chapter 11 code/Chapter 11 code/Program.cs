d1d72a743d9d1557ef7bd7214a3294094463ec79
Gant2134 <Sonic25100@yahoo.com>
2017-01-11 13:26:20 -0800

    discuss about encapsulation
    set public members to private

--- Tax Application/Chapter 11 code/Chapter 11 code/Chapter 11 code/Program.cs
+++ Tax Application/Chapter 11 code/Chapter 11 code/Chapter 11 code/Program.cs
@@ -16,14 +16,40 @@ namespace Chapter_11_code
             // the second part "new person" creating an object of the class "person"
             person p = new person();
 
-            p.m_FirstName = "Christopher";
-            p.m_LastName = "McCall";
-            p.m_age = 30;
+            // this version has the member variables set to private
+            // as a result the we no longer have "direct access to change the variables
+
+
+           // p.m_FirstName = "Christopher";
+           // p.m_LastName = "McCall";
+           // p.m_age = 30;
 
             // the line below will not compile because since we did not specify any access
             // modifier for our m_Haircolor, the defaul access is private, m_Haircolor member Variable
 
             //p.m_Haircolor = "Dark";
+
+            //the next line declares a reference to an object of the class person,
+            //but no actual object corresponds to that reference
+            
+
+            person p1;
+
+            //the line below does not work, since the reference "p1" does not match
+            //any exisiting object, we are trying to refer to something that doesnt exist
+            // note: that reference is refered to as a variable because a reference is 
+            // just a special type of variable.
+            // some other types such as integral types; (aka
+            // -int
+            // -float
+            // -double
+            // -string)
+            // this means they are both object and references at the same time 
+            //this means that there is no need to assign an object of type int
+            //to a reference of type int,
+            p1.m_age = 18;
+
+            
         }
     }
 }
